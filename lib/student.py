c4f0613c1322a3798b5eb0ac8d802b4eb0ee9e03
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-02-16 17:43:09 +0300

    using super

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,16 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name,knowledge=[]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, new_knowledge):
+        return self.knowledge.append(new_knowledge)
+
+
+
+student1 = Student('John', 'Doe')
+print(student1.first_name)
+print(student1.learn('Python programming'))
+print(student1.knowledge)
